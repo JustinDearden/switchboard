a56cd62a48ce00ad05e8bbfc47e74c729d42c6d7
Justin Dearden <deardenj@protonmail.com>
2025-10-14 23:17:39 -0400

    ShadCN Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div className="text-red-500 font-extrabold">Hello world</div>;
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }

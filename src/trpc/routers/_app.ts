5d9a9f67d6dd29a6646766be65de62aa7cf69f5e
Justin Dearden <deardenj@protonmail.com>
2025-10-18 22:43:55 -0400

    07: ai providers (#6)

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,19 +1,27 @@
-import { inngest } from "@/inngest/client";
-import { createTRPCRouter, protectedProcedure } from "../init";
-import prisma from "@/lib/db";
+import { inngest } from '@/inngest/client';
+import { createTRPCRouter, protectedProcedure } from '../init';
+import prisma from '@/lib/db';
+
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: 'execute/ai',
+    });
+
+    return { success: true, message: 'Job queued' };
+  }),
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
   createWorkflow: protectedProcedure.mutation(async () => {
     await inngest.send({
-      name: "test/hello.world",
+      name: 'test/hello.world',
       data: {
-        email: "test@test.com",
+        email: 'test@test.com',
       },
     });
 
-    return { success: true, message: "Job queued" };
+    return { success: true, message: 'Job queued' };
   }),
 });
 // export type definition of API

da14c3e303c5577c0c66fcf05fac347420b93906
Justin Dearden <deardenj@protonmail.com>
2025-10-21 00:20:38 -0400

    10: payments (#9)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import { polarClient } from "@/lib/polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,4 +12,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "9fdade3e-7852-4718-9b59-821824b58a74",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });

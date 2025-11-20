8ee03629c8552df74d9c9ee982921a0cc4ba3fc4
Justin Dearden <deardenj@protonmail.com>
2025-11-19 23:08:48 -0500

    Social Auth Providers (#30)

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -50,6 +50,38 @@ export function RegisterForm() {
     },
   });
 
+  const signInGithub = async () => {
+    await authClient.signIn.social(
+      {
+        provider: 'github',
+      },
+      {
+        onSuccess: () => {
+          router.push('/');
+        },
+        onError: (ctx) => {
+          toast.error('Something went wrong');
+        },
+      }
+    );
+  };
+
+  const signInGoogle = async () => {
+    await authClient.signIn.social(
+      {
+        provider: 'google',
+      },
+      {
+        onSuccess: () => {
+          router.push('/');
+        },
+        onError: (ctx) => {
+          toast.error('Something went wrong');
+        },
+      }
+    );
+  };
+
   const onSubmit = async (values: RegisterFormValues) => {
     await authClient.signUp.email(
       {
@@ -84,6 +116,7 @@ export function RegisterForm() {
               <div className='grid gap-6'>
                 <div className='flex flex-col gap-4'>
                   <Button
+                    onClick={signInGithub}
                     variant='outline'
                     className='w-full'
                     type='button'
@@ -98,6 +131,7 @@ export function RegisterForm() {
                     Continue with GitHub
                   </Button>
                   <Button
+                    onClick={signInGoogle}
                     variant='outline'
                     className='w-full'
                     type='button'
